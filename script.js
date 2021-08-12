894535177332fd22576d4a5c58c6b436faaf949d
scorpi078 <86363661+scorpi078@users.noreply.github.com>
2021-08-12 18:17:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,32 +5,3 @@ menu.addEventListener('click', function () {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
-
-$(document).ready(function () {
-  $('.submit').click(function (event) {
-   
-
-    var email = $('.email').val();
-    var subject = $('.subject').val();
-    var message = $('.message').val();
-    var statusElm = $('.status');
-    statusElm.empty();
-
-    if (email.includes('@') && email.includes('.')) {
-      statusElm.append('<div>Email is valid</div>');
-    } else {
-      statusElm.append('<div>Email is not valid</div>');
-      event.preventDefault();
-    }
-
-    if (subject.lenght < 1) {
-      statusElm.append('<div>Subject is not valid</div>');
-      event.preventDefault();
-    }
-
-    if (message.lenght < 1) {
-      statusElm.append('<div>Message box is not valid</div>');
-      event.preventDefault();
-    }
-  })
-})
