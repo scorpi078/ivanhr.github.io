1889d26fba1f3716754c28a20c0127a480afc0ce
Ivan Hristodorov <provan25@gmail.com>
2022-08-13 15:47:48 +0300

    projects update and carousel added

--- script.js
+++ script.js
@@ -5,3 +5,66 @@ menu.addEventListener('click', function () {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+document.querySelectorAll(".carousel").forEach((carousel) => {
+  const items = carousel.querySelectorAll(".carousel__item");
+  const buttonsHtml = Array.from(items, () => {
+    return `<span class="carousel__button"></span>`;
+  });
+
+  carousel.insertAdjacentHTML(
+    "beforeend",
+    `
+		    <div class="carousel__nav">
+			${buttonsHtml.join("")}
+		      </div>
+	  `
+  );
+
+  const buttons = carousel.querySelectorAll(".carousel__button");
+
+
+
+  buttons.forEach((button, i) => {
+    button.addEventListener("click", () => {
+      items.forEach((item) =>
+        item.classList.remove("carousel__item--selected")
+      );
+      buttons.forEach((button) =>
+        button.classList.remove("carousel__button--selected")
+      );
+
+      items[i].classList.add("carousel__item--selected");
+      button.classList.add("carousel__button--selected");
+    });
+  });
+
+  items[0].classList.add("carousel__item--selected");
+  buttons[0].classList.add("carousel__button--selected");
+
+  let i = 0;
+  setInterval(function () {
+
+    const selected = document.querySelectorAll(".carousel__item");
+    const btns = carousel.querySelectorAll(".carousel__button");
+
+    if (i > 1) {
+      i = 0;
+
+    }
+    selected.forEach((select) =>
+      select.classList.remove("carousel__item--selected")
+    );
+
+    btns.forEach((btn) =>
+      btn.classList.remove("carousel__button--selected")
+    );
+
+    selected[i].classList.add("carousel__item--selected");
+    btns[i].classList.add("carousel__button--selected");
+
+    i++;
+
+  }, 5000)
+
+});
